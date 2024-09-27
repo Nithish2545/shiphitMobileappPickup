9eabf7ccb3c0cefc6e130882dfda5ebcdb6870ae
NithishKumarFreelancer <nithishkumarfreelancer@gmail.com>
2024-09-27 17:10:32 +0530

    firestore

--- utility/googlescreen/apiURLs.js
+++ utility/googlescreen/apiURLs.js
@@ -1,3 +1,4 @@
+// firestore
 const API_URL =  "https://api.sheety.co/4057b982b999cdc3a2c47ac28ad20d4a/pickupdata/sheet1"
 const SHEETY_API_URL =  API_URL
 export default {sheetDB:API_URL,sheety:SHEETY_API_URL};
