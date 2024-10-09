a680745b19af4ea7f1a42a78a2dfbd4736aa4e9d
NithishKumarFreelancer <nithishkumarfreelancer@gmail.com>
2024-10-09 19:13:24 +0530

    testing

--- utility/googlescreen/apiURLs.js
+++ utility/googlescreen/apiURLs.js
@@ -1,3 +1,3 @@
-const API_URL = "https://api.sheety.co/75cc6a982d934c7d4ee40aaf4161d985/pickupdataChennai/sheet1"
+const API_URL = "https://api.sheety.co/c7c17cdd7ee5502b4889b4905d34af28/pickupdataChennai/sheet1"
 const SHEETY_API_URL =  API_URL
 export default {sheetDB:API_URL,sheety:SHEETY_API_URL};
