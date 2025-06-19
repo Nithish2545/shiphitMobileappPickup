f02d93241010f3471ecb19a72ebe764c789b5f6e
Thinkvance <thinkvance@gmail.com>
2025-06-19 19:14:36 +0530

    bug fixed!

--- Pickup/screens/PickupDetails.js
+++ Pickup/screens/PickupDetails.js
@@ -80,29 +80,26 @@ const PickupDetails = () => {
   };
 
   const pickImage = async () => {
-    const permissionResult = await MediaLibrary.requestPermissionsAsync();
-
-    if (!permissionResult.granted) {
-      return;
-    }
-
-    const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaType.Images,
-      exif: true,
-    });
-
-    if (!result.cancelled) {
-      setImage(result.assets[0].uri);
-      if (result.assets[0].exif) {
-        setMetadata(result.assets[0].exif);
-        setTimestamp(
-          formatToIST(result.assets[0].exif.DateTime) || "Unknown date"
-        );
-      } else {
-        setTimestamp(new Date().toString());
-      }
-      setPickupersonImage([result.assets[0].uri]); // Store the URI for later upload
-    }
+    // const permissionResult = await MediaLibrary.requestPermissionsAsync();
+    // if (!permissionResult.granted) {
+    //   return;
+    // }
+    // const result = await ImagePicker.launchImageLibraryAsync({
+    //   mediaTypes: ImagePicker.MediaTypeOptions.Images,
+    //   exif: true,
+    // });
+    // if (!result.cancelled) {
+    //   setImage(result.assets[0].uri);
+    //   if (result.assets[0].exif) {
+    //     setMetadata(result.assets[0].exif);
+    //     setTimestamp(
+    //       formatToIST(result.assets[0].exif.DateTime) || "Unknown date"
+    //     );
+    //   } else {
+    //     setTimestamp(new Date().toString());
+    //   }
+    //   setPickupersonImage([result.assets[0].uri]); // Store the URI for later upload
+    // }
   };
 
   // Modified to take folder and awbNumber as arguments for dynamic path
@@ -194,7 +191,7 @@ const PickupDetails = () => {
       }
 
       const result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaType.Images,
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
         allowsMultipleSelection: true,
         quality: 1,
       });
@@ -230,10 +227,11 @@ const PickupDetails = () => {
       setFormError("You must upload between 1 to 2 form images.");
       return false;
     }
-    if (PickupersonImage.length === 0 || PickupersonImage.length > 1) {
-      setFormError("Picture Is Required!");
-      return false;
-    }
+    
+    // if (PickupersonImage.length === 0 || PickupersonImage.length > 1) {
+    //   setFormError("Picture Is Required!");
+    //   return false;
+    // }
 
     if (!pickupWeight || !numberOfPackages) {
       setFormError("Pickup weight and number of packages are required.");
